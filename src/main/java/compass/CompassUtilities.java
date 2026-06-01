0d285f6f2a5de6695a88900535eff87bb5a72248
Mohit Raj <mrxmohit@amazon.com>
2026-06-01 07:42:52 +0000

    Updated release date for Babelfish v.6.0.0 alongwith checksum

--- src/main/java/compass/CompassUtilities.java
+++ src/main/java/compass/CompassUtilities.java
@@ -40,8 +40,8 @@ public class CompassUtilities {
 	public static boolean onLinux    = false;
 	public static String  onPlatform = uninitialized;
 
-	public static final String thisProgVersion      = "2026-04";
-	public static final String thisProgVersionDate  = "April 2026";
+	public static final String thisProgVersion      = "2026-06";
+	public static final String thisProgVersionDate  = "June 2026";
 	public static final String thisProgName         = "Babelfish Compass";
 	public static final String thisProgNameLong     = "Compatibility assessment tool for Babelfish for PostgreSQL";
 	public static final String thisProgNameExec     = "Compass";
